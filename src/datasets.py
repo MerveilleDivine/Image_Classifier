0f5c306906364dc579c7bdd04afede640e0ecf45
Mervine Muganguzi <mervinemuganguao@gmail.com>
2026-06-09 18:39:18 +0300

    fix: make datasets module re-export data loaders

--- src/datasets.py
+++ src/datasets.py
@@ -1,1 +1,20 @@
-"""Dataset loading and transform helpers."""
+"""Backward-compatible dataset module.
+
+Prefer importing from src.data_loaders in new code.
+"""
+
+from .data_loaders import (
+    build_dip_test_transform,
+    build_test_transform,
+    build_train_transform,
+    get_cifar10_datasets,
+    get_cifar10_loaders,
+)
+
+__all__ = [
+    "build_dip_test_transform",
+    "build_test_transform",
+    "build_train_transform",
+    "get_cifar10_datasets",
+    "get_cifar10_loaders",
+]
